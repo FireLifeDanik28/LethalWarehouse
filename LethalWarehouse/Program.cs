a127c00e42da10c1a46d917ea48fdf4c776d7c62
FireLifeDanik28 <lifedanik2006@gmail.com>
2025-04-24 22:07:56 +0200

    Dodanie przedmiotu i magazynu, program na razie nie działa xd

--- LethalWarehouse/Program.cs
+++ LethalWarehouse/Program.cs
@@ -1,5 +1,32 @@
-﻿//Mogło być równieź KuriozumWarehouse ale chce LethalWarehouse
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+//Mogło być równieź KuriozumWarehouse ale chce LethalWarehouse
 namespace LethalWarehouse
 {
-    //test commit
+    public static class Program
+    {
+        public class Item
+        {
+            public string Name { get; set; }
+            public double WeightKg { get; set; }
+            public int WeirdnessLevel { get; set; }
+            public bool IsFragile { get; set; }
+        }
+        public class Warehouse
+        {
+            //nie wiem. list
+        }
+        public static void Main()
+        {
+            //max pojemność magazynu
+            Console.WriteLine("Enter warehouse item capacity: ");
+            int maxCapacity = int.Parse(Console.ReadLine());
+            //max waga magazynu
+            Console.WriteLine("Enter warehouse max weight in kg: ");
+            double maxWeight = double.Parse(Console.ReadLine());
+
+            //Warehouse warehouse = new Warehouse(maxCapacity, maxWeight);
+        }
+    }
 }
