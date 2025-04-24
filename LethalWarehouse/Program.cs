b856c3f39be6af5cbe0956453a0adb578ab01a81
FireLifeDanik28 <lifedanik2006@gmail.com>
2025-04-24 23:32:55 +0200

    very shitty frontend without frontend in c#

--- LethalWarehouse/Program.cs
+++ LethalWarehouse/Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-//Mogło być równieź KuriozumWarehouse ale chce LethalWarehouse
+﻿//Mogło być równieź KuriozumWarehouse ale chce LethalWarehouse
 namespace LethalWarehouse
 {
     public static class Program
@@ -23,7 +20,7 @@ namespace LethalWarehouse
             //Desc 
             public string Desc()
             {
-                return $"{{\n\t\"name\": \"{Name}\",\n\t\"weight_kg\": {WeightKg},\n\t\"Weird 1-10\": {WeirdnessLevel},\n\t\"is_fragile\": \"{(IsFragile ? "YES" : "NO")}\n}}";
+                return $"{{\n\t\"name\": \"{Name}\",\n\t\"weight_kg\": {WeightKg},\n\t\"Weird 1-10\": {WeirdnessLevel},\n\t\"is_fragile\": {(IsFragile ? "YES" : "NO")}\n}}";
             }
         }
         public class Warehouse
@@ -47,7 +44,7 @@ namespace LethalWarehouse
                 if (CurrentTotalWeight + item.WeightKg > MaxTotalWeight)
                     return (false, "Error: This item weights too much(still less than ur mom lmao)");
                 if (item.IsFragile && item.WeirdnessLevel == 7 && CurrentItemCount >= Capacity / 2)
-                    return (false, "Error: This item is too dangerous, its strange level is 7 and it's fragile.\nNot possible to storage rn");
+                    return (false, "Error: This item is too dangerous, its weirdness level is 7 and it's fragile.\nNot possible to storage rn");
                 Items.Add(item);
                 return (true, "Item added succesfully ☺");
             }
@@ -87,8 +84,81 @@ namespace LethalWarehouse
             double maxWeight = double.Parse(Console.ReadLine());
             
             Warehouse warehouse = new Warehouse(capacity, maxWeight);
+            //Console.WriteLine("Storage created. add items thru frontend idgaf");
+            //Jeżeli frontend istnieje wtedy program działa do tej pory
+            //i reszte można wykomentować/wykasować
+            //else
+            while (true)
+            {
+                Console.WriteLine("\nLethalTerminal™ menu");
+                Console.WriteLine("1) Add item");
+                Console.WriteLine("2) Remove item");
+                Console.WriteLine("3) SELECT *(all)");
+                Console.WriteLine("4) SELECT fragile AND heavier than (treshold)");
+                Console.WriteLine("5) Get average strangeness ");
+                Console.WriteLine("0) Escape");
+                string choice = Console.ReadLine();
 
-            Console.WriteLine("Storage created. add items thru frontend idgaf");
+                switch (choice)
+                {
+                    case "1":
+                        Console.Write("Name: ");
+                        string name = Console.ReadLine();
+
+                        Console.Write("Weight (kg): ");
+                        double weight = double.Parse(Console.ReadLine());
+
+                        Console.Write("How weird (1-10): ");
+                        int weirdness = int.Parse(Console.ReadLine());
+
+                        Console.Write("Is it fragile? (true/false): ");
+                        bool fragile = bool.Parse(Console.ReadLine());
+
+                        Item newItem = new Item(name, weight, weirdness, fragile);
+                        var (success, message) = warehouse.AddItem(newItem);
+                        Console.WriteLine(message);
+                        break;
+
+                    case "2":
+                        Random random = new Random();
+                        int jk = random.Next(100);
+                        if (jk == 100)
+                            Console.Write("Say my name: ");
+                        else
+                            Console.Write("Type the name of the object to delete: ");
+                        string nameToRemove = Console.ReadLine();
+                        bool removed = warehouse.RemoveItem(nameToRemove);
+                        Console.WriteLine(removed ? "Item destroyed." : "404: Item not found.");
+                        break;
+
+                    case "3":
+                        warehouse.PrintAll();
+                        break;
+
+                    case "4":
+                        Console.Write("Enter treshold: ");
+                        double threshold = double.Parse(Console.ReadLine());
+                        var selectedItems = warehouse.GetFragileOrHeavy(threshold);
+                        foreach (var item in selectedItems)
+                        {
+                            Console.WriteLine(item.Desc());
+                        }
+                        break;
+
+                    case "5":
+                        double avg = warehouse.GetAverageWeirdness();
+                        Console.WriteLine($"Average weirdness: {avg:F2}");
+                        break;
+
+                    case "0":
+                        Console.WriteLine("THERE IS NO ESCAPE");
+                        return;
+
+                    default:
+                        Console.WriteLine("Unknown Option.");
+                        break;
+                }
+            }
         }
     }
 }
